ba815cd03bc8dd258972fefb28f9939ef25438ca
JUNGJIN <ojj824@naver.com>
2022-01-29 23:11:24 +0900

    Page 연결 및 TEST #2

---
+++ frontend/src/Pages/MovieInfoPage/MovieInfoPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MovieInfoPage() {
+  return <div>MovieInfoPage</div>;
+}
+
+export default MovieInfoPage;
---
+++ frontend/src/Pages/RankPage/RankPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RankPage() {
+  return <div>Rankpage</div>;
+}
+
+export default RankPage;
