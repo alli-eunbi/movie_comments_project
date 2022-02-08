0281afe19c23ac6346998249882cd4e06f3e3675
baegyeongtae <bkt584@gmail.com>
2022-02-09 02:10:33 +0900

    rankpage 기본 css 및 데이터 받아오기

--- frontend/src/Pages/RankPage/RankPage.js
+++ frontend/src/Pages/RankPage/RankPage.js
@@ -1,74 +1,107 @@
-import React, {useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { List, Avatar } from 'antd';
 import axios from 'axios';
 
+import styled from 'styled-components';
+import NavBar from '../../Components/NavBar/NavBar';
 
-export default function InfoPages() {
 
-  axios.get("http://localhost:5000/user-ranking")
-  .then((response)=> {
-    console.log(response)
-      if (Object.values(response.success) === true) {
-        const user_index = Object.values(response.temperature_rank.index);
-        const user_name = Object.values(response.temperature_rank.name);
-        const user_profile_img = Object.values(response.temperature_rank.profile_image);
-        const user_temperature = Object.values(response.temperature_rank.temperature);
-       console.log(user_index, user_name, user_profile_img, user_temperature);
-     }
+export default function RankPage() {
+
+  const [SearchTerm, setSearchTerm] = useState("")
+  const [userData, setUserData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async() => {
+        const result = await axios.get(`http://localhost:5000/user-ranking`);
+        setUserData(result.data.reviewNum_rank)
+        console.log(userData) 
     }
-  )
-  .catch((error)=> {
-      console.log(error)
-  })
+    fetchData();
+  },[])
 
-  const data = [
-    {
-      title: 'Ant Design Title 1',
-    },
-    {
-      title: 'Ant Design Title 2',
-    },
-    {
-      title: 'Ant Design Title 3',
-    },
-    {
-      title: 'Ant Design Title 4',
-    },
-    {
-      title: 'Ant Design Title 5',
-    },
-    {
-      title: 'Ant Design Title 6',
-    },
-    {
-      title: 'Ant Design Title 7',
-    },
-    {
-      title: 'Ant Design Title 8',
-    },
-    {
-      title: 'Ant Design Title 9',
-    },
-    {
-      title: 'Ant Design Title 10',
-    }
-  ];
+  const updateSearchTerm = (newSearchTerm) => {
+    setSearchTerm(newSearchTerm)
+  }
+
+  // const data = () => {
+
+  //   axios.get("http://localhost:5000/user-ranking")
+  //   .then((response)=> {
+  //     console.log(response)
+  //     console.log(response.data.reviewNum_rank)
+
+  //     const userData = response.data.reviewNum_rank;
+
+  //       if (response.data.success === true) {
+  //         return userData;
+  //       }
+  //     }
+  //   )
+  //   .catch((error)=> {
+  //       console.log(error)
+  //   })
+
+  // }
+
+  console.log(userData)
 
   return (
       <>
-        <div><h2>RankPage</h2></div>
-        <List
-          itemLayout="horizontal"
-          dataSource={data}
-          renderItem={item => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                title={<a href="https://ant.design">{item.title}</a>}
-              />
-            </List.Item>  
-          )}
-        />,
+        <NavBar
+            refreshFunction={updateSearchTerm}
+        />
+          <Inner>
+            <Box>
+              <h4>영광의 TOP 10</h4>
+              <Container>
+                <List
+                  itemLayout="horizontal"
+                  dataSource={userData}
+                  renderItem={item => (
+                    <List.Item>
+                      <List.Item.Meta
+                        avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                        title={item.name}
+                      />
+                    </List.Item>
+                  )}
+                />
+              </Container>
+            </Box>
+          </Inner>
       </>
   )
 };
+
+
+const Inner = styled.div`
+height: 100vh;
+display: flex;
+background-color: #1C2126;
+justify-content: center;
+align-items: center;
+font-size: 100px;
+font-family: 'Noto Sans KR', sans-serif;
+`;
+
+const Box = styled.div`
+width: 90%;
+height: 75%;
+padding-top: 100px;
+border-radius: 12px;
+background-color: #EEEEEE;
+box-shadow: rgba(0, 0, 0, 0.8) 0px 5px 15px;
+text-align: center;
+font-size: 50px;
+flex-wrap: wrap;
+flex-direction: column;
+padding: 25px;
+`;
+
+const Container = styled.div`
+display: grid;
+padding-top: 50px;
+grid-template-columns: repeat(2, 1fr);
+gap: 10px;
+`;
